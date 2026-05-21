98d50d4ddf4e58e1275d41ec61ab36dd2983aa69
TitouanLNE <titouan.machet@lne.fr>
2026-05-21 16:54:39 +0200

    Create flexible_tasks.py

---
+++ flexible_tasks.py
@@ -0,0 +1,22 @@
+import os
+from inspect_ai import task, task_with
+from inspect_evals.mbpp import mbpp
+from inspect_evals.class_eval import class_eval
+
+@task
+def my_custom_mbpp():
+    """Dynamically filters mbpp samples."""
+    sample_ids_raw = os.environ.get("HAWK_SAMPLE_IDS")
+    if sample_ids_raw:
+        target_ids = [int(x.strip()) for x in sample_ids_raw.split(",") if x.strip().isdigit()]
+        return task_with(mbpp(), sample_id=target_ids)
+    return mbpp()
+
+@task
+def my_custom_class_eval():
+    """Dynamically filters gsm8k samples."""
+    sample_ids_raw = os.environ.get("HAWK_SAMPLE_IDS")
+    if sample_ids_raw:
+        target_ids = [int(x.strip()) for x in sample_ids_raw.split(",") if x.strip().isdigit()]
+        return task_with(gsm8k(), sample_id=target_ids)
+    return gsm8k()
